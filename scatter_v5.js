74e69e68dd14a21acea6df8b9c5c1e3f4a0036f2
geekyguc <guillaume.champion@outlook.com>
2025-03-21 17:59:48 +0100

    Update scatter_v5.js

--- scatter_v5.js
+++ scatter_v5.js
@@ -10,8 +10,6 @@ const engine = new BABYLON.Engine(canvas, true, {
 const scene = new BABYLON.Scene(engine);
 scene.clearColor = new BABYLON.Color4(0, 0, 0, 1);
 
-const xr = scene.createDefaultXRExperienceAsync();
-
 var camera = new BABYLON.UniversalCamera("MyCamera", new BABYLON.Vector3(0, 1, 0), scene);
 camera.minZ = 0.0001;
 camera.attachControl(canvas, true);
@@ -127,6 +125,8 @@ sprite.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.Action
     labelSprites.push(sprite);
 });
 
+const xr = scene.createDefaultXRExperienceAsync();
+	
 scene.onBeforeRenderObservable.add(() => {
 	
 	updateSpritePositions();
