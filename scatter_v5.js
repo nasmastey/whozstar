cbffb90a56e73af8ab73aac0cdbb00a137790224
geekyguc <guillaume.champion@outlook.com>
2025-03-21 18:52:27 +0100

    Update scatter_v5.js

--- scatter_v5.js
+++ scatter_v5.js
@@ -1,6 +1,4 @@
 //http-server -c-1
-import "@babylonjs/loaders/glTF";
-
 const canvas = document.getElementById('renderCanvas');
 const engine = new BABYLON.Engine(canvas, true, {
     preserveDrawingBuffer: true,
